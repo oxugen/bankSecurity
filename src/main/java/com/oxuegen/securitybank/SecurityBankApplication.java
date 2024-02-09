a5e1e8a5180c998d0bb6f1bce36f2c000f1f082e
PhilipDubrovskiy <dubrovskay.7830@mail.ru>
2024-02-09 16:31:55 +0300

    Add new filter

--- src/main/java/com/oxuegen/securitybank/SecurityBankApplication.java
+++ src/main/java/com/oxuegen/securitybank/SecurityBankApplication.java
@@ -2,8 +2,10 @@ package com.oxuegen.securitybank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class SecurityBankApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/oxuegen/securitybank/config/ProjectSecurityConfig.java
+++ src/main/java/com/oxuegen/securitybank/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.oxuegen.securitybank.config;
 
+import com.oxuegen.securitybank.filter.RequestValidationBeforeFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -40,12 +42,15 @@ public class ProjectSecurityConfig {
                     }
                 })
                 .and()
-                .csrf().ignoringRequestMatchers("/register", "/contact").and().authorizeHttpRequests((requests) -> {
+                .csrf().ignoringRequestMatchers("/register", "/contact")
+                .and()
+                .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
+                .authorizeHttpRequests((requests) -> {
             requests
-                    .requestMatchers("/myAccount").hasAuthority("VIEWACCOUNT")
-                    .requestMatchers("/myBalance").hasAnyAuthority("VIEWACCOUNT", "VIEWBALANCE")
-                    .requestMatchers("/myLoans").hasAuthority("VIEWLOANS")
-                    .requestMatchers("/myCards").hasAuthority("VIEWCARDS")
+                    .requestMatchers("/myAccount").hasRole("USER")
+                    .requestMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
+                    .requestMatchers("/myLoans").hasRole("USER")
+                    .requestMatchers("/myCards").hasRole("USER")
                     .requestMatchers("/user").authenticated()
                     .requestMatchers("/notices", "/contact", "/register").permitAll();
         });
